9083069611044459ce76fa74b95b06401b6bf063
Sushanth555-hub <sushanthsushanth145@gmail.com>
2025-05-13 07:59:14 +0530

    added comment

--- armstnum.c
+++ armstnum.c
@@ -1,22 +1,30 @@
 //finding The Armstrong number
 #include <stdio.h>
 #include <math.h>
+
+//main function is starting
 int main(){
   int n,count=0,temp;
   double sum=0;
   printf("Enter the number ");
   scanf("%d",&n);
+
+  //counting The number of element
   temp=n;
   while(temp!=0){
     temp=temp/10;
     count++;
   }
+
+  //taking each number out and doing the operation
   temp=n;
   while(temp!=0){
     int rev=temp%10;
     sum=sum+pow(rev,count);
     temp=temp/10;
   }
+
+  //comparing with number and displaying
   if((int)(round(sum))==n){
     printf("Given Number is armstron number");
   }else{
